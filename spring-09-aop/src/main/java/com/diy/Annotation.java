f8f41a53c978339b71e1fc9fd953173d9df3ef5a
tan-liming <1693780296@qq.com>
2022-03-27 23:05:53 +0800

    Spring AOP实现的三种方式（注解，标签，实现接口），以及JDK动态代理代理的是接口

--- spring-09-aop/src/main/java/com/diy/Annotation.java
+++ spring-09-aop/src/main/java/com/diy/Annotation.java
@@ -22,11 +22,14 @@ public class Annotation {
     //在环绕增强中，我们可以给地暖管一个参数，代表我们要获取切入的点
     @Around("execution(* com.service.UserServiceImp.*(..))")
     public void around(ProceedingJoinPoint joinPoint) throws Throwable {
-        System.out.println("注解周围");
+        System.out.println("注解环绕");
 
+        //获得签名
+        System.out.println("签名:" + joinPoint.getSignature());
+        //执行方法
         Object proceed = joinPoint.proceed();
 
-        System.out.println("注解周围之后");
+        System.out.println("注解环绕之后");
     }
 
 }
--- spring-09-aop/src/main/java/com/diy/DiyPointcut.java
+++ spring-09-aop/src/main/java/com/diy/DiyPointcut.java
@@ -3,10 +3,10 @@ package com.diy;
 public class DiyPointcut {
 
     public void before(){
-        System.out.println("之前");
+        System.out.println("配置之前");
     }
 
     public void after(){
-        System.out.println("之后");
+        System.out.println("配置之后");
     }
 }
