33e1a60ccb13f06d8396056737eeb2d7ec270c64
tan-liming <1693780296@qq.com>
2022-04-11 22:55:13 +0800

    整合mybatis，未成功

--- spring-10-mybatis/src/main/java/com/mapper/UserMapper.java
+++ spring-10-mybatis/src/main/java/com/mapper/UserMapper.java
@@ -6,6 +6,4 @@ import java.util.List;
 
 public interface UserMapper {
     List<User> selectUser();
-
-
 }
--- spring-10-mybatis/src/test/java/Mytest.java
+++ spring-10-mybatis/src/test/java/Mytest.java
@@ -1,8 +1,16 @@
 import com.mapper.UserMapper;
 import com.pojo.User;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import org.apache.ibatis.io.Resources;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 
 public class Mytest {
@@ -17,4 +25,17 @@ public class Mytest {
             System.out.println(user);
         }
     }
+
+    @Test
+    public void test() throws IOException {
+        String resources = "mybatis-config.xml";
+        InputStream in = Resources.getResourceAsStream(resources);
+        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(in);
+        //自动提交事务用true
+        SqlSession sqlSession = sessionFactory.openSession(true);
+
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        List<User> userList = mapper.selectUser();
+    }
+
 }
